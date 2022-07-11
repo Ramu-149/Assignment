b38b468d055d9891d10e58c4021b695b7b8de2bc
Ramu E <eramu149@gmail.com>
2022-07-11 10:08:21 +0530

    fourth commit

---
+++ Migrations/20220711043311_Initial Migration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GtihubApp.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Emp",
+                columns: table => new
+                {
+                    EId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EName = table.Column<string>(nullable: true),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    EDesign = table.Column<string>(nullable: true),
+                    EDoj = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emp", x => x.EId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Emp");
+        }
+    }
+}
