aa621e082eb4cf5cf657a2f4036c14af9c64d688
Ramu E <eramu149@gmail.com>
2022-07-11 10:00:59 +0530

    third commit

---
+++ Data/EmpContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using GtihubApp.Models;
+
+    public class EmpContext : DbContext
+    {
+        public EmpContext (DbContextOptions<EmpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GtihubApp.Models.Emp> Emp { get; set; }
+    }
--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 
 namespace GtihubApp
 {
@@ -24,6 +25,9 @@ namespace GtihubApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<EmpContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("EmpContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
