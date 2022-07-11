820a5c7366b1f436f0c381cdfdb860db47e9735a
Ramu E <eramu149@gmail.com>
2022-07-11 09:56:16 +0530

    second commit

---
+++ Models/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GtihubApp.Models
+{
+    public class Emp
+    {
+        [Key]
+        public int EId { get; set; } 
+        public string EName { get; set; }
+        public int EmployeeId { get; set; }
+        public string EDesign { get; set; }
+        public DateTime EDoj { get; set; }
+    }
+}
